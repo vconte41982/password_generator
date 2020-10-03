9755c200674da2e78255f7405227af55b96faa59
vconte41982 <68178433+vconte41982@users.noreply.github.com>
2020-10-03 13:36:58 -0700

    updates code in JavaScript,ReadMe,screenshots

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # password_generator
+
+This is a password generator that shows a length of password generate by a comand. This is an assigmnet to show knowledge learned at UC Berkeley bootcamp
+
+## Technologies
+
+The following are the technologies, I used for this project; along with HTML,CSS provied by UC Berkeley bootcamp :
+
+and integration of functions and Chartset link <https://net-comber.com/charset.html> to generate a password
+
+## How to use it?
+
+This a password generator in which who has a desire of generate a password with lowercase letters, uppercase letters, numbers, symbols 
+will be able to create an unlimited arrange of passwords with just a press of a button.
+
+### Screenshots
+
+![screenshot] (/png_screenshots/screens1.png)
+![screenshot] (/png_screenshots/screens2.png)
+![screenshot] (/png_screenshots/screens3.png)
+![screenshot] (/png_screenshots/screens4.png)
+![screenshot] (/png_screenshots/screens5.png)
+![screenshot] (/png_screenshots/screens6.png)
+![screenshot] (/png_screenshots/screens7.png)
--- script.js
+++ script.js
@@ -1,66 +1,3 @@
-const passwordEl = document.getElementById("password");
-const lengthEl = document.getElementById("length");
-const lowercaseEl = document.getElementById("lowercase");
-const uppercaseEl = document.getElementById("uppercase");
-const numbersEl = document.getElementById("numbers");
-const specialcharactersEl = document.getElementById("specialcharacters");
-const generateEl = document.getElementById("generate");
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  numbers: getRandomNumbers,
-  SpecialCharacters: getRandomSpecialCharacters,
-};
-
-generateEl.addEventListener("click", () => {
-  const length = +lengthEl.value;
-  const hasLower = lowercaseEl.checked;
-  const hasUpper = uppercaseEl.checked;
-  const hasNumbers = numbersEl.checked;
-  const hasSpecialCharacters = specialcharactersEl.checked;
-
-  passwordEl.innerText = YourSecurePassword(
-    hasLower,
-    hasUpper,
-    hasNumbers,
-    hasSpecialCharacters,
-    length
-  );
-});
-function generatePassword(lower, upper, numbers, specialcharacters, length) {
-  let password = "";
-
-  const typesCount = lower + upper + numbers + specialcharacters;
-  console.log("typesCount:", typesCount);
-
-  const typesArr = [
-    { lower },
-    { upper },
-    { numbers },
-    { specialcharacters },
-  ].filter((item) => Object.values(item)[0]);
-
-  console.log("typesArray:", typesArr);
-  if (typesCount === 0) {
-    return "";
-  }
-
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0];
-      //console.log("funcName:", funcName);
-
-      password += randomFunc[funcName]();
-    });
-  }
-  const finalPassword = password.slice(0, length);
-  return finalPassword;
-}
-
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 //function writePassword() {
 //var password = generatePassword();
@@ -70,25 +7,70 @@ function generatePassword(lower, upper, numbers, specialcharacters, length) {
 
 //-//https://net-comber.com/charset.html
 
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateBtnEl = document.getElementById('generateBtn')
+const passwordEl = document.getElementById('password')
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+generateBtnEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasnNumber = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+
+  passwordEl.innerText = generatePassword(hasLower, hasUpper, hasnNumber, hasSymbols, length);
+})
+function generatePassword(lower, upper, number, symbol, length) {
+
+  let generatedPassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
+    (
+      item => Object.values(item)[0]
+    );
+
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumbers() {
+function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
-function getRandomSpecialCharacters() {
-  const SpecialCharacters = "!@#$%*&(){}[]=<>^?,.";
-  return SpecialCharacters[
-    Math.floor(Math.random() * SpecialCharacters.length)
-  ];
+function getRandomSymbol() {
+  const symbols = '!@#$%*&(){}[]=<>^?/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+
